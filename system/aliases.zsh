76b7da76b7ed36cf3f56940389474657697b0617
anatolyg <anatoly@geyfman.net>
2013-10-01 22:11:08 -0700

    added my own aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,3 +8,13 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+alias scpresume="rsync -avr --partial --progress --rsh=ssh"
+alias treeview="ls -R | grep \":$\" | sed -e 's/:$//' -e 's/[^-][^\/]*\//–/g' -e 's/^/ /' -e 's/-/|/'"
+
+alias storage="ssh 192.168.1.111"
+
+alias ebayify="mogrify -resize 1600x1600"
+
+alias dg_dev="ssh -p 2222 grid@dev.dicomgrid.com './getLatest.sh'"
+alias dg_uat="ssh ubuntu@uat.dicomgrid.com './getLatest.sh'"
