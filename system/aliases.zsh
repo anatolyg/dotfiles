f8413b46f4d50449908ecab76bc37fab7397729c
anatolyg <anatoly@geyfman.net>
2013-10-01 22:16:45 -0700

    added server shortcuts

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,18 +3,28 @@
 #   `brew install coreutils`
 if $(gls &>/dev/null)
 then
-  alias ls="gls -F --color"
+  alias ls="gls -Fltr --color"
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
 
+# command shortcuts
 alias scpresume="rsync -avr --partial --progress --rsh=ssh"
 alias treeview="ls -R | grep \":$\" | sed -e 's/:$//' -e 's/[^-][^\/]*\//–/g' -e 's/^/ /' -e 's/-/|/'"
 
+# server shortcuts
 alias storage="ssh 192.168.1.111"
 
+# DG
+alias dg-dev="ssh -p 2222 grid@dev.dicomgrid.com './getLatest.sh'"
+alias dg-uat="ssh ubuntu@uat.dicomgrid.com './getLatest.sh'"
+
+# VT
+alias vt-ec2="ssh -i ~/.ssh/verdande.pem ubuntu@ec2-54-214-147-101.us-west-2.compute.amazonaws.com"
+
+# CP
+
+# tools
 alias ebayify="mogrify -resize 1600x1600"
 
-alias dg_dev="ssh -p 2222 grid@dev.dicomgrid.com './getLatest.sh'"
-alias dg_uat="ssh ubuntu@uat.dicomgrid.com './getLatest.sh'"
