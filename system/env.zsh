601109b6207daed4f5e537a1baadbeb507002394
Anatoly Geyfman <anatoly@geyfman.net>
2013-10-09 11:03:57 -0700

    added mvim as default editor

--- system/env.zsh
+++ system/env.zsh
@@ -1,5 +1,6 @@
 # Only set this if we haven't set $EDITOR up somewhere else previously.
 if [[ "$EDITOR" == "" ]] ; then
   # Use sublime for my editor.
-  export EDITOR='subl'
+  export EDITOR='mvim'
 fi
+export PROMPT_COMMAND='echo -ne "\033]0;$PWD\007"'
