2e8ca3d8e07703fe81ffff419b0e405266ef522e
anatolyg <anatoly@geyfman.net>
2013-11-07 15:43:35 -0800

    better status export

--- exec/batterystatus.sh
+++ exec/batterystatus.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # first get the battery info section, then split it by ' = ', take the second part, replace '=' with : === valid JSON
-ioreg -l -w 0 | grep BatteryInfo | awk -F" = " '{ print $2 }' | sed 's/=/\:/g'
+ioreg -l -w 0 | grep BatteryInfo | awk -F" = " '{ print "\{\"batteryStatus\"\:",$2,"\,\"date\":",d,"}," }' "d=$(date +%s)" | sed 's/=/\:/g' >> /Users/anatoly/.stats/battery.json
